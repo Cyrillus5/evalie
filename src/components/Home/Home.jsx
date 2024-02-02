d79fa77739cdcf6480f189d133ca79a55b4f99cd
Cyrillus <chenetcyril@free.fr>
2024-02-02 23:20:41 +0100

    add css modifications to homePage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,8 +2,6 @@ import { useNavigate } from 'react-router-dom';
 
 import './Home.css';
 
-import pictureHomePage from '/house-evalie.png';
-
 function Home () {
     const navigate = useNavigate();
 
@@ -14,15 +12,12 @@ function Home () {
     return (
         <div className='Home'>
             <div className='Home-card'>
-                <img src={pictureHomePage} className='Home-card-picture'/>                
                 <div className='Home-card-main'>
                     <h1 className='Home-card-main-title'>Rénovez votre logement</h1>  
                     <p className='Home-card-main-text'>Des aides financières existent pour vous <br/>aider à renover votre logement</p>
                     <button onClick={handleWorksClick} className='Home-card-main-button'>Quelles aides pour mes travaux ?</button>   
-                </div>
-                
-            </div>
-            
+                </div>                
+            </div>            
         </div>
     )
 }
