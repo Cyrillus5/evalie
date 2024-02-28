f3c2e3e53343baabe6f7fe5cf0829812b73eba7e
Cyrillus <chenetcyril@free.fr>
2024-02-28 22:50:07 +0100

    add robots.txt file

--- src/components/Line/Line.jsx
+++ src/components/Line/Line.jsx
@@ -1,7 +1,13 @@
+import PropTypes from 'prop-types';
+
 function Line({ className }) {
     return (
         <div className={ className }></div>
     )
 }
+
+Line.propTypes = {
+    className: PropTypes.string.isRequired,
+};
 
 export default Line;
